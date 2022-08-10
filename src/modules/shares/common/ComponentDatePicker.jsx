3f22bee8c1ad1ff19555a27ee6d75c38c89b1718
phuongdao <drac19021501bb@gmail.com>
2022-08-10 20:13:38 +0700

    fix component date picker

--- src/modules/shares/common/ComponentDatePicker.jsx
+++ src/modules/shares/common/ComponentDatePicker.jsx
@@ -1,8 +1,6 @@
 import * as React from 'react';
 import { DesktopDatePicker } from '@mui/x-date-pickers/DesktopDatePicker';
 import { TextField, InputLabel } from '@mui/material';
-import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 
 const ComponentDatePicker = (props) => {
     const [value, setValue] = React.useState(props.date);
@@ -15,7 +13,6 @@ const ComponentDatePicker = (props) => {
     return (
         <>
             <InputLabel>{props.label}</InputLabel>
-            <LocalizationProvider dateAdapter={AdapterDateFns}>
             <DesktopDatePicker
                 readOnly={props.isReadOnly ? true : false}
                 inputFormat="MM/dd/yyyy"
@@ -23,7 +20,6 @@ const ComponentDatePicker = (props) => {
                 onChange={handleChange}
                 renderInput={(params) => <TextField {...params} fullWidth />}>
             </DesktopDatePicker>
-            </LocalizationProvider>
         </>
     )
 }
