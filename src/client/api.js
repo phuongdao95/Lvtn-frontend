4f7b6c5f236588fa684b635812c66a4e4bf7301f
anhphuong0204 <47697482+anhphuong0204@users.noreply.github.com>
2022-08-18 21:16:02 +0700

    Add centralize client

---
+++ src/client/api.js
@@ -0,0 +1,27 @@
+import axios from 'axios';
+
+const BASE_URL = "https://localhost:7115/";
+
+const api = axios.create({
+    baseURL: BASE_URL,
+    headers: {
+        Accept: 'application/json',
+        crossdomain: true
+    }
+});
+
+api.interceptors.request.use((onRequestSuccess) => {
+    // TODO: add Token to request
+    // Fake Token
+    const authorizationToken = "SomeToken";
+
+    if (authorizationToken) {
+        onRequestSuccess.headers = {
+            ...onRequestSuccess.headers,
+            'Authorization': authorizationToken
+        };
+    }
+    return onRequestSuccess;
+});
+
+export default api;
