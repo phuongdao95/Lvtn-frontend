57c06c824910e365b729b617a2c34b190619485b
phuongdao <drac19021501bb@gmail.com>
2022-09-28 23:46:41 +0700

    add required in label

--- src/components/DialogForm/Label.jsx
+++ src/components/DialogForm/Label.jsx
@@ -1,7 +1,7 @@
 import { Typography } from "@mui/material"
 import { grey } from "@mui/material/colors"
 
-export default function Label({ text }) {
+export default function Label({ text, required }) {
     return <Typography
         sx={{
             fontWeight: "550",
@@ -9,7 +9,7 @@ export default function Label({ text }) {
             fontSize: 14
         }}
     >
-        {text}
+        {text}{required ? " *" : "" }
     </Typography>
 
 }
